059a154c368ba999245943700fc02c3bc42bc5a5
zhangyifan <zhangyifan@rongcloud.cn>
2022-08-19 13:02:16 +0800

    feat: release 5.2.4
    Change-Id: I56f4cbb87eb0d0fba87a8f65e8590babd0a778ce

--- example/lib/app_data.dart
+++ example/lib/app_data.dart
@@ -1,8 +1,8 @@
 class AndroidPushInfo {
-  static const idMI = '2882303761520168950';
-  static const appKeyMI = '5922016842950';
-  static const appIdMeizu = '149077';
-  static const appKeyMeizu = '3d4774146be34805933bcde641b397de';
-  static const appKeyOPPO = 'ff6b79dc6f5846e381a846b6fbcebea7';
-  static const appSecretOPPO = 'c8ed6e53be9441f89a2c1aececd8b64f';
+  static const idMI = '';
+  static const appKeyMI = '';
+  static const appIdMeizu = '';
+  static const appKeyMeizu = '';
+  static const appKeyOPPO = '';
+  static const appSecretOPPO = '';
 }
--- example/lib/app_data_github.dart
+++
@@ -1,8 +0,0 @@
-class AndroidPushInfo {
-  static const idMI = '';
-  static const appKeyMI = '';
-  static const appIdMeizu = '';
-  static const appKeyMeizu = '';
-  static const appKeyOPPO = '';
-  static const appSecretOPPO = '';
-}
